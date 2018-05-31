66c95af295098b2f437caa441e37df230d6d65fb
sterehov <sterehov@localhost>
2018-05-31 18:04:36 +0300

    Добавлена документация

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+/**
+ * Базовые компоненты
+ */
+module.exports.base = require('./lib/base')
+/**
+ * Атомарные элементы
+ */
 module.exports.atom = require('./lib/atom')
+/**
+ * Арифметика
+ */
 module.exports.arithmetic = require('./lib/arithmetic')
